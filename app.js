7a0561ccea0eb00d92784b68dfa33c17cfa78dc8
yukito-onuma <147116904+yukito-onuma@users.noreply.github.com>
2024-05-29 12:53:39 +0900

    ｋｊ

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 function janken(choice, number) {
     let resultElement = document.getElementById("ResultMessage");
-    let imageElement = document.getElementById("AgentImage");
+    let resultContainer = document.getElementById("result");
+    resultContainer.innerHTML = ""; // 結果をリセット
 
-    let random = Math.floor(Math.random() * 10) + 1;
-    let agent = Num2Agent(random);
-    let agentImageURL = getAgentImageURL(random);
+    for (let i = 0; i < number; i++) {
+        let random = Math.floor(Math.random() * 10) + 1;
+        let agent = Num2Agent(random);
 
-    for (let i = 0; i < number; i++) 
-    {
-        resultElement.innerHTML = "選ばれたエージェント: " + agent;
-        imageElement.src = agentImageURL;
+        let resultMessage = document.createElement("p");
+        resultMessage.textContent = "選ばれたエージェント: " + agent;
+        resultContainer.appendChild(resultMessage);
     }    
 }
 
@@ -53,46 +53,4 @@ function Num2Agent(random) {
     }
 
     return agent;
-}
-
-function getAgentImageURL(random) {
-    let url = "";
-
-    switch (random) {
-        case 1:
-            url = "jett.png";
-            break;
-        case 2:
-            url = "images/phoenix.png";
-            break;
-        case 3:
-            url = "images/sage.png";
-            break;
-        case 4:
-            url = "images/killjoy.png";
-            break;
-        case 5:
-            url = "images/brimstone.png";
-            break;
-        case 6:
-            url = "images/viper.png";
-            break;
-        case 7:
-            url = "images/omen.png";
-            break;
-        case 8:
-            url = "images/reyna.png";
-            break;
-        case 9:
-            url = "images/cypher.png";
-            break;
-        case 10:
-            url = "images/sova.png";
-            break;
-        default:
-            url = "images/default.png";
-            break;
-    }
-
-    return url;
 }
