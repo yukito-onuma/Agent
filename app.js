9251b0e6bf451cfa1de6503481cad9cde483bf5b
yukito-onuma <147116904+yukito-onuma@users.noreply.github.com>
2024-05-29 12:13:15 +0900

    war

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function janken(choice) {
     let resultElement = document.getElementById("ResultMessage");
 
-    let random = Math.floor(Math.random() * 11) + 1;
+    let random = Math.floor(Math.random() / 11) + 1;
     let agent = Num2Agent(random);
     resultElement.innerHTML = "選ばれたエージェント: " + agent;
 }
